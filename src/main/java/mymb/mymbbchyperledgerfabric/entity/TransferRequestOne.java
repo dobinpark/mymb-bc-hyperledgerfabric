d7c2f21b542ecb67aa0751c48b8a63a0e1042779
DOBIN <devpark0810@gmail.com>
2024-05-25 12:14:02 +0900

    [UPDATE] TokenService update

--- src/main/java/mymb/mymbbchyperledgerfabric/entity/TransferRequestOne.java
+++ src/main/java/mymb/mymbbchyperledgerfabric/entity/TransferRequestOne.java
@@ -3,12 +3,11 @@ package mymb.mymbbchyperledgerfabric.entity;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Data
 public class TransferRequestOne {
 
     private String from;
     private String to;
-    private List<String> tokenNumber;
+    private ArrayList<String> tokenNumber;
 }
